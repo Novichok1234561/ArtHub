2fd490fd686180779228b0fd720157252a76e52a
Novichok1234561 <artem1582910@gmail.com>
2025-04-13 06:09:10 +0300

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,77 +1,159 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/CoolManYesYes/OrionLib/refs/heads/main/source.txt')))()
-local Window = OrionLib:MakeWindow({
-    Name = "Q1nly",
-    HidePremium = false,
-    SaveConfig = false,           
-    ConfigFolder = nil,       
-    IntroEnabled = true,
-    IntroText = "Welcome, " .. game.Players.LocalPlayer.Name,
-    IntroIcon = "rbxassetid://82778010291487"
-})
-local Tab = Window:MakeTab({
-	Name = "Help",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddSlider({
-	Name = "Slider",
-	Min = 16,
-	Max = 1000,
-	Default = 5,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "speed",
-	Callback = function(Value)
+local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
+local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/SaveManager.luau"))()
+local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/InterfaceManager.luau"))()
+ 
+local Window = Library:CreateWindow{
+    Title = `Fluent {Library.Version}`,
+    SubTitle = "by Actual Master Oogway",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(500, 400),
+    Resize = true, -- Resize this ^ Size according to a 1920x1080 screen, good for mobile users but may look weird on some devices
+    MinSize = Vector2.new(470, 380),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.RightControl -- Used when theres no MinimizeKeybind
+}
+local Tabs = {
+    Main = Window:CreateTab{
+        Title = "Main",
+        Icon = "phosphor-users-bold"
+    },
+    Settings = Window:CreateTab{
+        Title = "Settings",
+        Icon = "settings"
+    }
+}
+local Slider = Tabs.Main:CreateSlider("Slider", {
+    Title = "Speed Hack",
+    Description = "Speed power",
+    Default = 16,
+    Min = 16,
+    Max = 5000,
+    Rounding = 1,
+    Callback = function(Value)
          game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-	end    
+    end
 })
-Tab:AddSlider({
-	Name = "Jump Huck",
-	Min = 50,
-	Max = 2000,
-	Default = 5,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "jump power",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-	end    
+local Slider = Tabs.Main:CreateSlider("Slider", {
+    Title = "Jump Power",
+    Description = "Jump power",
+    Default = 50,
+    Min = 50,
+    Max = 5000,
+    Rounding = 1,
+    Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+    end
 })
-OrionLib:MakeNotification({
-	Name = "Q1nly",
-	Content = "Hello my friend",
-	Image = "rbxassetid://4483345998",
-	Time = 2
-})
-Tab:AddButton({
-	Name = "Fly",
-	Callback = function()
-      		loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "Noclip",
-	Callback = function()
-      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Mirgarr/noclip-script-roblox/refs/heads/main/Noclip"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "Esp",
-	Callback = function()
-      		local Esp = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/x114/RobloxScripts/main/OpenSourceEsp"))()
--- Boxes --
-Esp.Box = true
-Esp.BoxColor = Color3.fromRGB(255,255,255)
-Esp.BoxOutline = true
-Esp.BoxOutlineColor = Color3.fromRGB(0,0,0)
--- HealthBars --
-Esp.HealthBar = false
-Esp.HealthBarSide = "Left" -- Left,Bottom,Right
--- Names --
-Esp.Names = true
-Esp.NamesColor = Color3.fromRGB(255,255,255)
-Esp.NamesOutline = true
-Esp.NamesFont = 2
-Esp.NamesSize = 13
-  	end    
-})
+Library:Notify{
+    Title = "Notification",
+    Content = "Hello!",
+    SubContent = "Hello my friend!", -- Optional
+    Duration = 3 -- Set to nil to make the notification not disappear
+}
+Tabs.Main:CreateButton{
+    Title = "Noclip",
+    Description = "Noclip Script",
+    Callback = function()
+        Window:Dialog{
+            Title = "Title",
+            Content = "This is a dialog",
+            Buttons = {
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/Mirgarr/noclip-script-roblox/refs/heads/main/Noclip"))()
+                {
+                    Title = "Confirm",
+                    Callback = function()
+                        print("Confirmed the dialog.")
+                    end
+                },
+                {
+                    Title = "Cancel",
+                    Callback = function()
+                        print("Cancelled the dialog.")
+                    end
+                }
+            }
+        }
+    end
+}
+Tabs.Main:CreateButton{
+    Title = "Fly",
+    Description = "Fly script",
+    Callback = function()
+        Window:Dialog{
+            Title = "Title",
+            Content = "This is a dialog",
+            Buttons = {
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+                {
+                    Title = "Confirm",
+                    Callback = function()
+                        print("Confirmed the dialog.")
+                    end
+                },
+                {
+                    Title = "Cancel",
+                    Callback = function()
+                        print("Cancelled the dialog.")
+                    end
+                }
+            }
+        }
+    end
+}
+
+local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Esp", Default = false })
+    Toggle:OnChanged(function(Value)
+        if Value == true then
+            _G.espits = true
+            for i, v in pairs(game.Workspace:GetDescendants()) do
+                if v:FindFirstChild("Highlight") then
+                    --nichego    
+
+                else
+                    
+                    if v:FindFirstChild("Humanoid")then
+                        local esp = Instance.new("Highlight", v)
+                        esp.FillColor = Color3.fromRGB(17, 164, 255)                                         
+                    end
+                end
+            end
+        else
+            _G.espits = false
+            for i, v in pairs(game.Workspace:GetDescendants()) do
+                if v:FindFirstChild("Highlight") then
+                    v.Highlight:Destroy()
+                end
+            end
+        end
+    end)
+
+    Players.PlayerAdded:Connect(function(player)
+        player.CharacterAdded:Connect(function(character)
+            if _G.espits == true then
+                local esp = Instance.new("Highlight", character)
+                esp.FillColor = Color3.fromRGB(17, 164, 255)
+            end
+        end)
+    end)
+
+    Players.PlayerAdded:Connect(onPlayerAdded)
+
+    local function onCharacterAdded(character)      
+        if _G.espits == true then
+            local esp = Instance.new("Highlight", character)
+            esp.FillColor = Color3.fromRGB(17, 164, 255)
+        end
+    end
+
+    local function onPlayerAdded(player)
+        player.CharacterAdded:Connect(onCharacterAdded)      
+        if player.Character then
+            onCharacterAdded(player.Character)
+        end
+    end
+    
+    for _, player in pairs(Players:GetPlayers()) do
+        onPlayerAdded(player)
+    end
+    
